3632f789fd5298f928ceca0b9bce955ebb5ed33c
Eloi Poch <eloi.poch@gmail.com>
2013-11-12 22:47:08 +0100

    Etude 3-2

---
+++ src/code/ch03-02/geom.erl
@@ -0,0 +1,19 @@
+%%% @author Eloi Poch <eloi.poch@gmail.com>
+%%% @doc Functions for calculating areas of geometric shapes.
+%%%
+%%% @version 0.2
+
+-module(geom).
+-export([area/3]).
+
+
+%% @doc Calculates the area of a shape (rectangle, triangle
+%% and ellipse)
+-spec(area(atom(), number(), number()) -> number()).
+
+area(rectangle, Length, Width) when Length >= 0, Width >= 0 ->
+  Length * Width;
+area(triangle, Base, Height) when Base >= 0, Height >= 0 ->
+  Base * Height / 2.0;
+area(ellipse, MajorRadius, MinorRadius) when MajorRadius >= 0, MinorRadius >= 0 ->
+  math:pi() * MajorRadius * MinorRadius.
